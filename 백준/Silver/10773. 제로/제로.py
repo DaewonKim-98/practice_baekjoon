d9d392aaf6080dece6090699e8a07e2166fbe645
harry5414 <139425563+DaewonKim-98@users.noreply.github.com>
2023-08-10 17:51:52 +0900

    [Silver IV] Title: 제로, Time: 3772 ms, Memory: 32036 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,18 @@
+K = int(input())
+
+stack = []
+for i in range(K):
+    N = int(input())
+    # 만약 N이 0일 때
+    if N == 0:
+        # 만약 stack에 원소가 있을 때 지운다.
+        if len(stack) != 0:
+            stack.pop()
+        # 만약 stack에 원소가 없으면 지나간다.
+        else:
+            pass
+    # N이 0이 아닐 때
+    else:
+        stack.append(N)
+
+print(sum(stack))
