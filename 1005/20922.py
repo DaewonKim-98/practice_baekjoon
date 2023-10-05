956370ee9ba2b3d8d2198eb9643bc0c7377b59b0
daewon <harry5414@naver.com>
2023-10-05 22:30:39 +0900

    1005

--- 1005/20922.py
+++ 1005/20922.py
@@ -5,24 +5,34 @@ arr = list(map(int, input().split()))
 # 원소를 인덱스로 하는 0으로된 리스트를 만들어두고 시작점과 끝점을 증가시키면서 끝까지 가게끔
 # 원소가 K개 이상 되면 시작점이 움직이고 이하면 끝점이 움직이면서 최댓값 찾기
 
-idx_list = [0] * 200000
+idx_list = [0] * 100001
 start = 0
 end = 0
 max_length = 0
-idx_list[arr[0]] = 1
-while end < N - 1:
-    # print(start, end)
-    # 만약 원소 개수가 K보다 많아지면 start 옮기기
-    if idx_list[arr[end]] > K:
-        # start를 옮길 것이므로 그 부분 원소 하나 삭제
+while end < N:
+    # 원소의 개수가 K개 이하이면 end를 늘리고 원소 추가
+    while end < N and idx_list[arr[end - 1]] <= K:
+        # print(end)
+        idx_list[arr[end]] += 1
+        end += 1
+
+    # 원소의 개수가 K개가 넘어가면 end 다시 이전으로
+    end -= 1
+    # end가 끝에 도달했으면
+    if end == N - 1 and idx_list[arr[end]] <= K:
+        end += 1
+        max_length = max(max_length, end - start)
+        break
+    
+    # 수열 길이 갱신
+    max_length = max(max_length, end - start)
+    
+    # 원소의 개수가 K개 이하가 될 때까지 start +1
+    while start <= end and idx_list[arr[end]] > K:
         idx_list[arr[start]] -= 1
         start += 1
-    # 아니면 end 계속 옮기기
-    else:
-        end += 1
-        idx_list[arr[end]] += 1
-    # 최대 길이 갱신
-    if max_length < end - start:
-        max_length = end - start
-
+    
+    end += 1
+    
+    
 print(max_length)
